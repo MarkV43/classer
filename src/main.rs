36b08a1561acae57a2b0d04a25869f2c5210310d
MarkV43 <marcelovogt43@gmail.com>
2025-11-02 22:32:13 -0300

    Added button and a few UI elements

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,86 @@
+use std::time::Duration;
+
 use ggez::{
     glam::Vec2,
-    graphics::{Canvas, Color, Drawable},
+    graphics::{Canvas, Color, DrawMode, DrawParam, FillOptions, Mesh, Rect},
     *,
 };
 
+struct Button {
+    rect: Rect,
+    text: String,
+    callback: fn() -> (),
+}
+
+impl Button {
+    fn update(&mut self, ctx: &mut Context) -> Result<(), GameError> {
+        if ctx
+            .mouse
+            .button_just_pressed(winit::event::MouseButton::Left)
+        {
+            let pos = ctx.mouse.position();
+
+            if self.rect.contains(pos) {
+                (self.callback)();
+            }
+        }
+
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context, canvas: &mut Canvas) -> Result<(), GameError> {
+        let mesh =
+            ggez::graphics::Mesh::new_rectangle(ctx, DrawMode::fill(), self.rect, Color::GREEN)?;
+
+        canvas.draw(&mesh, DrawParam::new());
+
+        let text = ggez::graphics::Text::new(&self.text);
+
+        canvas.draw(
+            &text,
+            Vec2::new(self.rect.x + 10.0, self.rect.center().y - 10.0),
+        );
+
+        Ok(())
+    }
+}
+
 struct State {
-    dt: std::time::Duration,
+    dt: Duration,
+    btn: Button,
 }
 
 impl event::EventHandler for State {
     fn update(&mut self, ctx: &mut Context) -> Result<(), GameError> {
         self.dt = ctx.time.delta();
+
+        self.btn.update(ctx)?;
+
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> Result<(), GameError> {
         let mut canvas = Canvas::from_frame(ctx, Color::BLACK);
+        let (w, _) = ctx.gfx.drawable_size();
 
-        let text = ggez::graphics::Text::new(format!("{:?}", self.dt));
+        let mouse_pos = ctx.mouse.position();
+
+        let rect = ggez::graphics::Mesh::new_rectangle(
+            ctx,
+            DrawMode::fill(),
+            Rect::new(0.0, 0.0, w, mouse_pos.y),
+            Color::RED,
+        )?;
+
+        canvas.draw(&rect, DrawParam::new());
+
+        let fps = 1.0 / self.dt.as_secs_f64();
+
+        let text = ggez::graphics::Text::new(format!("{fps:.4}"));
 
         canvas.draw(&text, Vec2::new(100.0, 100.0));
+
+        self.btn.draw(ctx, &mut canvas)?;
 
         canvas.finish(ctx)?;
 
@@ -30,6 +91,11 @@ impl event::EventHandler for State {
 fn main() {
     let state = State {
         dt: std::time::Duration::new(0, 0),
+        btn: Button {
+            rect: Rect::new(5.0, 5.0, 40.0, 40.0),
+            text: "+".to_owned(),
+            callback: || println!("Botão clicado"),
+        },
     };
 
     let c = conf::Conf::new();
